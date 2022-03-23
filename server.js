574e11c7eb362356b3af91494ba73858097e7d6d
kelvin711 <arauzkelvin99@gmail.com>
2022-03-23 13:30:21 -0500

    rest api for goals

--- server.js
+++ server.js
@@ -5,18 +5,14 @@ const express = require("express"),
     { errorHandler } = require("./server/middleware/errorMiddleware")
     port = process.env.PORT,
     cors = require("cors"),
+    connectDB = require("./server/config/db"),
     server = app.listen( port, () => console.log(`all systems firing on port ${port}`));
 
-//app.use("/api/goals", require("./server/routes/goalRoutes"))
-
-// app.get("/api/goals", (req, res) => {
-//     res.status(200).json({ message: "Goals" })
-// })
 app.use(cors());//will allow the backend to send and recieve information
 app.use(express.json());//parse json data
 app.use(express.urlencoded({extended:true}));//parse string data
 
-// require("./server/config/database.config");//getting from the config file
+
+connectDB()//running our DB
 require("./server/routes/goalRoutes")(app);//injecting the app for our routes
 app.use(errorHandler)
-// //injecting the route into the server
--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,37 +1,48 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require("../models/goalModel")
 
 module.exports = {
     showAllGoals: asyncHandler(async (req, res) => {
-        res.status(200).json({ message: "Still Goal og" })
-        // Product.find() //mongoose built in queries
-        //     .then( Products => res.json({ allProducts : Products}))//Products is all of the prods, then 
-        //     //responding with a json object and putting it in the allProducts variable
+        const goals = await Goal.find()
+        res.status(200).json(goals)
     }),
+
     createGoal: asyncHandler(async (req,res) => { //create is the restful pattern
         if (!req.body.text) {
             res.status(400)
             throw new Error("Please add a text field")
         }
-        res.status(200).json({ message: "Set Goal baby og" })
-        // Product.create(req.body) //mongoose built in queries
-        //     .then( newlyCreatedProduct => res.json({ createProduct : newlyCreatedProduct }))
-        //     .catch( err => res.json(err.errors));
+        const goal = await Goal.create(req.body)
+        res.status(200).json(goal)
     }),
+
     updateGoal: asyncHandler(async (req, res) => {
-        res.status(200).json({ message: `Update that goal og ${req.params.id}` })
-        // Product.findOneAndUpdate({ _id: req.params.id }, req.body, {new : true})
-        //     .then( updatedProduct => res.json({ updateProduct: updatedProduct }))
-        //     .catch( err => res.json(err.errors));
+        const goal = await Goal.findById(req.params.id)
+        if(!goal) {
+            res.status(400)
+            throw new Error("Goal not found")
+        }
+        const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(200).json(updatedGoal)
     }), 
+
     destroyGoal: asyncHandler(async (req, res) => {
-        res.status(200).json({ message: `Delete goal kinda sad tho og ${req.params.id}` })
-        // Product.deleteOne({ _id: req.params.id })
-        // .then( ProductToDelete => res.json({ ProductToDelete : ProductToDelete }))
-        // .catch(err => res.json(err.errors));
+        const goal = await Goal.findById(req.params.id)
+        if(!goal) {
+            res.status(400)
+            throw new Error("Goal not found")
+        }
+        await goal.remove()
+        res.status(200).json({id: req.params.id})
     }),
-    show: asyncHandler(async (req, res) => {
-        Product.findOne({ _id: req.params.id})
-            .then( oneProduct => res.json({ showProduct : oneProduct }))
-            .catch( err => res.json(err.errors));
+
+    showOneGoal: asyncHandler(async (req, res) => {
+        const goal = await Goal.findById(req.params.id)
+        if(!goal) {
+            res.status(400)
+            throw new Error("Goal not found")
+        }
+        res.status(200).json(goal)
     }),
+    
 }
